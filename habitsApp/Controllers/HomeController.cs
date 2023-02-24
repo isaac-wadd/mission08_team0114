f7fd9cddf3c75b022f708f6dbcea6c77dcbe7a1b
Nathan Conrad <nathan.conrad2020@gmail.com>
2023-02-24 14:02:17 -0700

    edit to add.cshtml

--- habitsApp/Controllers/HomeController.cs
+++ habitsApp/Controllers/HomeController.cs
@@ -3,6 +3,12 @@
 © 2023 IS Junor Core Group 01-14
 ASP.NET controller for habitsApp
 Author: Isaac Waddell
+
+NOTE: labels shown for each of the Create, Read, Update, and Delete functionalities, e.g.
+"
+C { ––––––––––––––––––––
+} ––––––––––––––––––––––
+"
 */
 
 using System;
@@ -15,108 +21,116 @@ using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using habitsApp.Models;
 
-namespace habitsApp.Controllers {
-    public class HomeController : Controller {
+namespace habitsApp.Controllers
+{
+    public class HomeController : Controller
+    {
         private readonly ILogger<HomeController> _logger;
 
-// ASSUMPTION: 'TaskCtxt' will be the name of the class as created in the 'habitsApp.Models' namespace
+        // ASSUMPTION: 'TaskCtxt' will be the name of the class as created in the 'habitsApp.Models' namespace
         private TaskCtxt _taskCtxt { get; set; }
 
-        public HomeController(ILogger<HomeController> logger, TaskCtxt taskCtxt) {
+        public HomeController(ILogger<HomeController> logger, TaskCtxt taskCtxt)
+        {
             _logger = logger;
 
-// the private variable as it will be initialized in the constructor for 'HomeController'
+            // the private variable as it will be initialized in the constructor for 'HomeController'
             _taskCtxt = taskCtxt;
         }
 
-        public IActionResult Index() { return View(); }
+        // R { –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-        public IActionResult Index() {
-            //var tasks = _tastkCtxt.tasks.ToList();
-            return View();
+        public IActionResult Index()
+        {
+            var tasks = _tastkCtxt.tasks.ToList();
+            return View(tasks);
         }
 
-// } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+        // } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-/*
-NOTES ON 'ViewBag':
-    - the 'pageTitle' variable can be used to set some text in the view (not necessary, so let me know)
-    - the 'formAction' should be used in the view's 'form' element's 'asp-action' attribute;
-        - this facilitates various processes since we can:
-            1. change which action is triggered dynamically and therefore
-            2. use the same view for both adding and updating records
-        - this can be done by referencing the object (and its attributes) in the cshtml file,
-          e.g. '@ViewBag.pageTitle'
-*/
-// C { –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-// ASSUMPTION: 'Task' view will exist for both adding and editing (updating) records
+        /*
+        NOTES ON 'ViewBag':
+            - the 'pageTitle' variable can be used to set some text in the view (not necessary, so let me know)
+            - the 'formAction' should be used in the view's 'form' element's 'asp-action' attribute;
+                - this facilitates various processes since we can:
+                    1. change which action is triggered dynamically and therefore
+                    2. use the same view for both adding and updating records
+                - this can be done by referencing the object (and its attributes) in the cshtml file,
+                  e.g. '@ViewBag.pageTitle'
+        */
+        // C { –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+        // ASSUMPTION: 'Task' view will exist for both adding and editing (updating) records
         [HttpGet]
-        public IActionResult Add() {
+        public IActionResult Add()
+        {
             ViewBag.categories = _taskCtxt.categories.ToList();
             ViewBag.pageTitle = "Add";
-// 'formAction' here will be set to 'add' so that the 'Add' post action (see below) is triggered
-// this is an example of what's described above in the 'NOTES ON ViewBag'
+            // 'formAction' here will be set to 'add' so that the 'Add' post action (see below) is triggered
+            // this is an example of what's described above in the 'NOTES ON ViewBag'
             ViewBag.formAction = "Add";
-            return View();
+            return View("Task");
         }
 
         [HttpPost]
-        public IActionResult Add(Task task) {
-            if (ModelState.IsValid) {
+        public IActionResult Add(Tasks task)
+        {
+            if (ModelState.IsValid)
+            {
                 _taskCtxt.Add(task);
                 _taskCtxt.SaveChanges();
-                return View("TaskGrid");
+                return View("Index");
             }
-            else { return View( task); }
+            else { return View("Task", task); }
         }
-// } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+        // } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-// R { –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
-// ASSUMPTION: 'TaskGrid' view will exist, rather than just having it on the home (index) page
-        public IActionResult TaskGrid() { return View(); }
-// } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
-// U { –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+        // U { –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
         [HttpGet]
-        public IActionResult Edit(int taskID) {
+        public IActionResult Edit(int taskID)
+        {
             var task = _taskCtxt.tasks.Single(t => t.taskID == taskID);
             ViewBag.categories = _taskCtxt.categories.ToList();
             ViewBag.pageTitle = "Edit";
             ViewBag.formAction = "Edit";
-            return View("Add", task);
+            return View("Task", task);
         }
 
         [HttpPost]
-        public IActionResult Edit(Task task) {
-            if (ModelState.IsValid) {
+        public IActionResult Edit(Tasks task)
+        {
+            if (ModelState.IsValid)
+            {
                 _taskCtxt.Update(task);
                 _taskCtxt.SaveChanges();
-                return View("TaskGrid");
+                return View("Index");
             }
-            else {
+            else
+            {
                 ViewBag.categories = _taskCtxt.categories.ToList();
                 return View(task);
             }
         }
-// } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+        // } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-// D { –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+        // D { –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
         [HttpGet]
-        public IActionResult Delete(int taskID) {
-            var task = _taskCtxt.tasks.Single(t = t.taskID = taskID);
+        public IActionResult Delete(int taskID)
+        {
+            var task = _taskCtxt.tasks.Single(t => t.taskID == taskID);
             return View(task);
         }
 
         [HttpPost]
-        public IActionResult Delete(Task task) {
+        public IActionResult Delete(Tasks task)
+        {
             _taskCtxt.Remove(task);
             _taskCtxt.SaveChanges();
-            return View("TaskGrid");
+            return View("Index");
         }
-// } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+        // } –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
+        public IActionResult Error()
+        {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
